153a2ba2c1de055da8b4df2886f77365d07a0260
GuilhermeViana16 <guilhermeviana347@gmail.com>
2019-09-25 16:17:03 -0300

    alteracao 25/09

--- src/app/compra/compra.component.ts
+++ src/app/compra/compra.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CarrinhoItem } from 'src/app/loja-detail/carrinho-compra/carrinho-compra.model'
+import { CarrinhoCompraService } from 'src/app/loja-detail/carrinho-compra/carrinho-compra.service'
 
 @Component({
   selector: 'app-compra',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompraComponent implements OnInit {
 
-  constructor() { }
+  constructor(private compraService: CarrinhoCompraService) { }
 
   ngOnInit() {
   }
 
+  carrinhoItems(): CarrinhoItem[] {
+    return this.compraService.carrinhoItems()
+  }
+
+  increaseQty(item: CarrinhoItem){
+    this.compraService.increaseQty(item)
+  }
+
+ decreaseQty(item: CarrinhoItem){
+   this.compraService.decreaseQty(item)
+ }
+
+ remove(item: CarrinhoItem){
+   this.compraService.removeItem(item)
+ }
 }
---
+++ src/app/compra/compra.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { CarrinhoCompraService } from '../loja-detail/carrinho-compra/carrinho-compra.service'
+import { CarrinhoItem } from '../loja-detail/carrinho-compra/carrinho-compra.model'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CompraService {
+
+  constructor(private carrinhoService: CarrinhoCompraService) { }
+
+  carrinhoItems(): CarrinhoItem[]{
+    return this.carrinhoService.items
+  }
+
+  increaseQty(item: CarrinhoItem){
+    this.carrinhoService.increaseQty(item)
+  }
+
+ decreaseQty(item: CarrinhoItem){
+   this.carrinhoService.decreaseQty(item)
+ }
+
+
+ remove(item: CarrinhoItem){
+   this.carrinhoService.removeItem(item)
+ }
+}
